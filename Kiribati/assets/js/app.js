1a3940a559e4dae26fdd63e21c66e42e7bd7f225
deviirmr <deviirmr@gmail.com>
2018-09-27 16:39:23 +0200

    MaxZoom:16 is updatad for base layer

--- Kiribati/assets/js/app.js
+++ Kiribati/assets/js/app.js
@@ -92,11 +92,12 @@ function sizeLayerControl() {
 /* Basemap Layers */
 
 var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-maxZoom: 10,
+maxZoom: 16,
 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  maxZoom: 16,
 attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
